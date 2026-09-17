2f250a908bc06bdf861f9721824cde6c35bb9309
Gustavo Lima <141791764+santanadegustavo@users.noreply.github.com>
2026-09-16 21:49:46 -0300

    botoes de prev e next feitos
    agora, eu dei interatividade para os botoes de next e prev

---
+++ scripts/scripts.js
@@ -0,0 +1,43 @@
+const prevButton = document.getElementById("prev")
+const nextButton = document.getElementById("next")
+const items = document.querySelectorAll(".item")
+const dots = document.querySelectorAll(".dot")
+const numberIndicator = document.querySelector(".numeros")
+const lista = document.querySelector(".lista")
+
+let ativo = 0
+const total = items.length
+let timer
+
+function update(direcao){  
+    document.querySelector('.item.ativo').classList.remove('ativo')
+    document.querySelector('.dot.ativo').classList.remove('ativo')
+
+    if(direcao > 0){
+        ativo = ativo + 1
+
+        if(ativo === total){
+            ativo = 0
+        }
+    } else if(direcao < 0){
+        ativo = ativo -1
+
+        if(ativo <0){
+            ativo = total -1
+        }
+    }
+
+    items[ativo].classList.add('ativo')
+    dots[ativo].classList.add('ativo')
+
+}
+
+prevButton.addEventListener ('click', function(){
+
+    update(-1)
+})
+
+nextButton.addEventListener ('click', function(){
+
+    update(1)
+})
